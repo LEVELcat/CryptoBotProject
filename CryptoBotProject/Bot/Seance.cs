f7d66c628fe8acddf0da9d7c54b8e31363d2480b
LEVEL_cat <stalkerclearsky2000@gmail.com>
2022-12-19 09:22:26 +0300

    Remove SeanceKeeper class

--- CryptoBotProject/Bot/Seance.cs
+++ CryptoBotProject/Bot/Seance.cs
@@ -2,6 +2,14 @@
 {
     public class Seance
     {
+        private static Dictionary<long, Seance> instances = new Dictionary<long, Seance>();
 
+        public static Seance GetSeance(long id)
+        {
+            if (instances.ContainsKey(id) == false)
+                instances.Add(id, new Seance());
+
+            return instances[id];
+        }
     }
 }
