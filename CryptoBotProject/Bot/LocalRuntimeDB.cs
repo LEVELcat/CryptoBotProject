4e9bc85083531e4bc237b72179932422e14349c7
LEVEL_cat <stalkerclearsky2000@gmail.com>
2023-01-10 10:23:58 +0300

    localRuntimeDbUpdate

--- CryptoBotProject/Bot/LocalRuntimeDB.cs
+++ CryptoBotProject/Bot/LocalRuntimeDB.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CryptoBotProject.WebParse;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,34 @@ namespace CryptoBotProject.Bot
 {
     internal class LocalRuntimeDB
     {
+        private static LocalRuntimeDB instance;
+        public static LocalRuntimeDB Instance
+        {
+            get
+            {
+                instance ??= new LocalRuntimeDB();
+                return instance;
+            }
+        }
+
+        private LocalRuntimeDB()
+        { }
+
+        public bool GetData( )
+        {
+
+        }
+
+        public static MySqlCommand BuildMySqlCommand(string ,)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+
+
+
+
+
+        }
+
 
     }
 }
